99a7a21d06cb8e7739df7e60803ec8b8406e44f7
Amine Gaddah <aminegaddah@192.168.1.3>
2026-06-10 21:19:20 +0100

    playwright yml updated auth create

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,11 +21,14 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html'],['allure-playwright']],
+  expect: {
+    timeout: 10000,        
+  },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -36,6 +39,8 @@ export default defineConfig({
     screenshot: 'only-on-failure',
     video: 'on-first-retry',
     testIdAttribute: 'data-qa',
+    actionTimeout: 15000,
+    navigationTimeout: 30000,
   },
 
   /* Configure projects for major browsers */
